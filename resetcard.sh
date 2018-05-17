9f24d9bea668627fa0d1d44bb485b422032e35fb
itaasa <34899051+itaasa@users.noreply.github.com>
2018-05-17 14:11:58 +0000

    Delete resetcard.sh

--- resetcard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-INTERFACE="$(iw dev | awk '$1=="Interface" {print $2}')"
-
-ifconfig $INTERFACE down
-iwconfig $INTERFACE mode managed
-ifconfig $INTERFACE up
-
-echo "Network card: {$INTERFACE} is no longer in monitor mode."
